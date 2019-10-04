c0c4998f965f07065089058d96997da796425627
MaxtorCoder <warsongkiller.s8@gmail.com>
2019-10-04 20:44:03 +0200

    Add Loot Rules and Add ServerGroupRemove

---
+++ Source/NexusForever.WorldServer/Game/Group/Static/LootRule.cs
@@ -0,0 +1,10 @@
+﻿namespace NexusForever.WorldServer.Game.Group.Static
+{
+    public enum LootRule
+    {
+        FreeForAll      = 0x00,
+        RoundRobin      = 0x01,
+        NeedBeforeGreed = 0x02,
+        Master          = 0x03
+    }
+}
---
+++ Source/NexusForever.WorldServer/Game/Group/Static/LootRuleHarvest.cs
@@ -0,0 +1,8 @@
+﻿namespace NexusForever.WorldServer.Game.Group.Static
+{
+    public enum LootRuleHarvest
+    {
+        RoundRobin  = 0x0,
+        FirstTagger = 0x1
+    }
+}
