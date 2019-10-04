8376fd682ee77a57ae0c7080f34f0da16d36a541
Albert Varaksin <albeva@me.com>
2019-10-04 19:28:41 +0100

    Group Leave WIP

---
+++ Source/NexusForever.WorldServer/Network/Message/Model/ClientGroupLeave.cs
@@ -0,0 +1,17 @@
+﻿using NexusForever.Shared.Network;
+using NexusForever.Shared.Network.Message;
+using NexusForever.WorldServer.Game.Group.Static;
+
+namespace NexusForever.WorldServer.Network.Message.Model
+{
+    [Message(GameMessageOpcode.ClientGroupLeave)]
+    public class ClientGroupLeave : IReadable
+    {
+        public ulong GroupId { get; set; }
+
+        public void Read(GamePacketReader reader)
+        {
+            GroupId = reader.ReadULong();
+        }
+    }
+}
