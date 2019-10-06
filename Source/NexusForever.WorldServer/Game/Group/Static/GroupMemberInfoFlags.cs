090af2cf206f3281b998cbc59417f119a93433e7
MaxtorCoder <warsongkiller.s8@gmail.com>
2019-10-06 15:37:05 +0200

    Add all flags

--- Source/NexusForever.WorldServer/Game/Group/Static/GroupMemberInfoFlags.cs
+++ Source/NexusForever.WorldServer/Game/Group/Static/GroupMemberInfoFlags.cs
@@ -10,11 +10,17 @@ namespace NexusForever.WorldServer.Game.Group.Static
         CanInvite       = 1 << 1,
         CanKick         = 1 << 2,
         Disconnected    = 1 << 3,
+        Pending         = 1 << 4,
+        Tank            = 1 << 5,
+        Healer          = 1 << 6,
+        DPS             = 1 << 7,
+        MainTank        = 1 << 8,
+        MainAssist      = 1 << 9,
+        RaidAssistant   = 1 << 10,
+        Ready           = 1 << 11,
+        RoleLocked      = 1 << 12,
         CanMark         = 1 << 13,
-        // RaidAssistant    = ?
-        // MainTank         = ?
-        // MainAssist       = ?
-        // RoleLocked       = ?
+        HasSetReady     = 1 << 14,
 
         GroupMember     = CanMark,
         GroupAdmin      = CanInvite | CanKick | CanMark
